f4ad73497c17aa689a034b22be864c2ef70f9724
yemoecom <32086127+yemoecom@users.noreply.github.com>
2020-11-16 15:09:36 +0800

    Update getshop.php

--- getshop.php
+++ getshop.php
@@ -8,7 +8,7 @@ $mange=new Mange();
 //data 参数url转后没转的都可以！没转的记得转后的双引号去掉
 $h5api='https://h5api.m.taobao.com/h5/mtop.taobao.wsearch.appsearch/1.0/?jsv=2.5.1&appKey=12574478&t=1605063873688&sign=1614fef44d5cacd241ea23d5534710db&api=mtop.taobao.wsearch.appSearch&v=1.0&H5Request=true&AntiCreep=true&type=jsonp&timeout=3000&dataType=jsonp&callback=mtopjsonp6&data=%7B%22m%22%3A%22shopitemsearch%22%2C%22vm%22%3A%22nw%22%2C%22sversion%22%3A%224.6%22%2C%22shopId%22%3A%22219465251%22%2C%22sellerId%22%3A%223074861492%22%2C%22style%22%3A%22wf%22%2C%22page%22%3A%221%22%2C%22sort%22%3A%22_coefp%22%2C%22catmap%22%3A%22%22%2C%22wirelessShopCategoryList%22%3A%22%22%7D';
 
-$r=array(); //data里参数
+$r=array(); //data里参数  没有参数为空否则提示登录
 $info=$mange->getejson($h5api,$r);
 if(empty($info)) echo '不支持此接口！';
 echo '<pre>';
